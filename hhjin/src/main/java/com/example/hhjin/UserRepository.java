0eca924073c1daf5ef1d9df922199584b871140f
dhslrl321 <dhslrl321@gmail.com>
2024-02-03 18:45:05 +0900

    commit

--- hhjin/src/main/java/com/example/hhjin/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.hhjin;
-
-import java.time.LocalDateTime;
-import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    List<User> findAllByXXXCondition(LocalDateTime from, LocalDateTime to);
-}
